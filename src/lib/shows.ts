2bda7d55dfac9aec81e96dd180b7c638359773a6
dprasen <dprasen@gmail.com>
2025-12-19 13:30:04 +0530

    updated shows to big list

--- src/lib/shows.ts
+++ src/lib/shows.ts
@@ -4,7 +4,7 @@ import type { Show, ShowsData } from '@/constants';
 
 const SHEET_ID = process.env.GOOGLE_SHEETS_ID!;
 const API_KEY = process.env.GOOGLE_SHEETS_API_KEY!;
-const SHEET_NAME = 'Sheet1'; // change if needed
+const SHEET_NAME = process.env.GOOGLE_SHEETS_NAME!;
 
 export async function fetchShows(): Promise<ShowsData> {
   const url =
